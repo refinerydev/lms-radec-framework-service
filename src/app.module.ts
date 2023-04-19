1e1735c1fc7b03c9cc0c7c7c9c78d64fe3df856f
iqbal167 <muhammadiqbalali167@gmail.com>
2023-04-19 17:43:25 +0700

    add: user module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './config/app/config.module';
 import { PrismaModule } from './providers/prisma/prisma.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [AppConfigModule, PrismaModule],
+  imports: [AppConfigModule, PrismaModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/users/users.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { UsersRepository } from './users.repository';
+import { PrismaService } from '../../providers/prisma/prisma.service';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository, PrismaService],
+})
+export class UsersModule {}
